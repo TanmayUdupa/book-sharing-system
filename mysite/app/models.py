e542ab5f550cbf3000dd445d5510f17e64c70470
TanmayUdupa <tanmaypraveenudupa@gmail.com>
2023-10-20 11:50:53 +0530

    Connect POSTGRESQL database

--- mysite/app/models.py
+++ mysite/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Book(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")

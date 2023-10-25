72bd602312c1d711e56214cc9bd344abd3b303d5
TanmayUdupa <tanmaypraveenudupa@gmail.com>
2023-10-25 12:06:50 +0530

    Add foreign key constraint to some fields

--- mysite/app/models.py
+++ mysite/app/models.py
@@ -19,37 +19,37 @@ class Book(models.Model):
     title = models.CharField(default = "Harry Potter", max_length = 50)
     author = models.CharField(default = "J. K. Rowling", max_length = 50)
     isbn = models.CharField(default = "ISBN", max_length = 50)
-    genre_id = models.IntegerField(default = 1)
+    genre_id = models.ForeignKey(Genre, on_delete=models.CASCADE)
     publication_year = models.IntegerField(default = 1)
     description = models.TextField(default = "Nice", max_length = 200)
     # cover_image = models.ImageField(upload_to="/images")
-    owner_id = models.IntegerField(default = 1)
+    owner_id = models.ForeignKey(User, on_delete=models.CASCADE)
     condition_desc = models.CharField(default = 'Good', max_length = 50)
     status = models.CharField(default = "Available", max_length=50)
     location = models.TextField(default = "India", max_length = 200)
 
 class BookReview(models.Model):
-    reviewer_id = models.IntegerField()
-    book_id = models.IntegerField()
+    reviewer_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
     review_text = models.TextField(max_length=200)
     rating = models.IntegerField()
 
 class Wishlist(models.Model):
-    user_id = models.IntegerField()
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     # List_of_Book_ID = 
     last_updated = models.DateField()
     is_public = models.BooleanField()
 
 class ShippedTo(models.Model):
     status = models.CharField(max_length = 50)
-    book_id = models.IntegerField()
+    book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
     from_address = models.TextField(max_length = 200)
     to_address = models.TextField(max_length=200)
     transaction_type = models.CharField(max_length = 50)
 
 class RequestsToBorrow(models.Model):
-    borrower_id = models.IntegerField()
+    borrower_id = models.ForeignKey(User, on_delete=models.CASCADE)
     request_date = models.DateField()
-    book_id = models.IntegerField()
+    book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
     status = models.CharField(max_length = 60)
     due_date = models.DateField()

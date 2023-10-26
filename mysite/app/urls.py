fa39c6f9b6e8bef332675dd057e4b6a59b3bd04b
True-Fox <pes1202100682@pesu.pes.edu>
2023-10-26 19:55:43 +0530

    Create django forms for login/signup

--- mysite/app/urls.py
+++ mysite/app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.home, name="home"),
     path('user_login/', views.user_login, name='user_login'),
-    path('user_signup/',views.user_signup,name="user_signup")
+    path('user_signup/',views.user_signup,name="user_signup"),
+    path('feed/',views.feed,name='feed')
 ]
--- mysite/app/views.py
+++ mysite/app/views.py
@@ -1,14 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.template import loader
 from .models import User
-from .forms import NameForm
+from .forms import LoginForm, SignUpForm
 
 def home(request):
     return render(request, "app/home.html")
 
-def user_login(request):
-    return render(request,"app/signin.html")
+def feed(request):
+    return render(request, 'app/feed.html')
 
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            print(email+" : "+ password)
+            return redirect('feed')
+    else:
+        form = LoginForm()
+
+    return render(request, 'app/signin.html', {'form': form})
 def user_signup(request):
-    return render(request, "app/signup.html")
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+    else:
+            form = SignUpForm()
+    return render(request, "app/signup.html",{'form':form})
+

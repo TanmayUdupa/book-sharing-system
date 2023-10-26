ac3edfb710dd3b2d87c1593beb66db15d60b23b5
True-Fox <pes1202100682@pesu.pes.edu>
2023-10-26 14:25:54 +0530

    Add Login/Signup

--- mysite/app/urls.py
+++ mysite/app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('',views.home, name="home"),
     path('user_login/', views.user_login, name='user_login'),
+    path('user_signup/',views.user_signup,name="user_signup")
 ]

629b9e366bad8161d9b71c948ce3284c5587c196
TanmayUdupa <tanmaypraveenudupa@gmail.com>
2023-10-27 10:46:54 +0530

    Integrate with DB, Encrypt password, Authenticate user

--- mysite/app/views.py
+++ mysite/app/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import User
 from .forms import LoginForm, SignUpForm
+from django.contrib.auth.hashers import make_password, check_password
 
 def home(request):
     return render(request, "app/home.html")
@@ -11,17 +12,30 @@ def feed(request):
     return render(request, 'app/feed.html')
 
 def user_login(request):
+    error = 0
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
             email = form.cleaned_data['email']
-            password = form.cleaned_data['password']
-            print(email+" : "+ password)
-            return redirect('feed')
+            x = None
+            for i in User.objects.all():
+                if (email == i.email):
+                    x = i
+                    break
+            if (x):
+                password = form.cleaned_data['password']
+                check = check_password(password, x.password)
+                if check:
+                    return redirect('feed')
+                else:
+                    error = 1
+            else:
+                error = 1
     else:
         form = LoginForm()
 
-    return render(request, 'app/signin.html', {'form': form})
+    return render(request, 'app/signin.html', {'form': form, 'error': error})
+
 def user_signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
@@ -29,6 +43,10 @@ def user_signup(request):
             name = form.cleaned_data['name']
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
+            encrypted_password = make_password(password)
+            user = User(name = name, email = email, password = encrypted_password)
+            user.save()
+            return(redirect('user_login'))
     else:
             form = SignUpForm()
     return render(request, "app/signup.html",{'form':form})
